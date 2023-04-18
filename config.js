d6cf400486f5e42c7949bbafec01856e71f59d7a
Hoàng An <120107696+CaoHoangAn@users.noreply.github.com>
2023-04-18 20:39:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối cuối tuần này anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/messages/t/100012321640287' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://m.me/an.caohoang.33' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
